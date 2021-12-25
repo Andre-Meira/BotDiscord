6eedc2ad04162c9179bd96b3b23dd8f8cdc49e88
andre meira <andre._.meira@hotmail.com>
2021-12-24 20:25:40 -0400

    funcionalidades para bot rodar.

--- BotDiscord/Program.cs
+++ BotDiscord/Program.cs
@@ -1,1 +1,58 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using BotDiscord.Services.Hosted;
+using Discord.Addons.Hosting;
+using Discord.WebSocket;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+namespace BotDiscord;
+
+class Program 
+{
+    static async Task Main(string[] args)
+        {
+            var config1 = new ConfigurationBuilder()
+                    .SetBasePath(Directory.GetCurrentDirectory())
+                    .AddJsonFile("appsettings.json", optional: true)
+                    .Build();
+
+            var builder = new HostBuilder()
+                .ConfigureAppConfiguration(appConfig =>
+                {
+                    var configuration = new ConfigurationBuilder()
+                        .SetBasePath(Directory.GetCurrentDirectory())
+                        .AddJsonFile("appsettings.json", false, true)
+                        .Build();
+                    appConfig.AddConfiguration(configuration);
+                })
+                .ConfigureLogging(ConfigLoggin =>
+                {
+                    ConfigLoggin.AddConsole();
+                    ConfigLoggin.SetMinimumLevel(LogLevel.Debug);
+                })
+                .ConfigureDiscordHost((context, config) =>
+                {
+                    config.SocketConfig = new DiscordSocketConfig
+                    {
+                        LogLevel = Discord.LogSeverity.Debug,
+                        AlwaysDownloadUsers = false,
+                        MessageCacheSize = 200,
+                    };
+                    config.Token = context.Configuration["TokenBot"];
+                })
+                .UseCommandService((context, config) =>
+                {
+                    config.CaseSensitiveCommands = false;
+                    config.LogLevel = Discord.LogSeverity.Debug; 
+                    config.DefaultRunMode = Discord.Commands.RunMode.Sync;
+                })
+                .ConfigureServices((context, services) =>
+                {                                           
+                    services.AddHostedService<CommandHandler>();                   
+                }).UseConsoleLifetime();
+
+            var host = builder.Build();
+
+            await host.RunAsync();
+    }
+}
