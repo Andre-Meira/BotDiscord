5ace9504b275e1088912f6efed1122d0dce5fdf3
Andre Meira <andre.meira@amaggi.com.br>
2022-04-13 17:51:12 -0400

    Update Channel e ajuste para enviar mensagem para RemoverStreamer

--- BotDiscord/Controller/Commands/ServCommands.cs
+++ BotDiscord/Controller/Commands/ServCommands.cs
@@ -20,30 +20,55 @@ public class ServCommands : ModuleBase<SocketCommandContext>
     public async Task RegisterChannel()
     {
         try
-        {            
+        {
             Discordserver discordserver = new Discordserver((long)Context.Guild.Id,
                 (long)Context.Channel.Id, Context.Guild.Name, Context.Channel.Name);
-            
+
             await _discServer.AddChannelAsync(discordserver);
             await ReplyAsync("Canal de notificação cadastrado!!");
 
-        }catch(Exception err)
+        }
+        catch (Exception err)
         {
             await ReplyAsync(err.Message);
         }
-    }    
+    }
+
+    [Command("UpdateChannelNotification")]
+    public async Task UpdateChannel()
+    {
+        try
+        {
+            Discordserver discordserver = await _discServer.GetChannel((long)Context.Guild.Id);
+
+            if (discordserver == null)
+                await ReplyAsync("Esse Servidor não tem um canal de notificação!!");
+            else
+            {
+                discordserver.IdChanel = (long)Context.Channel.Id;
+                discordserver.NameChannel = Context.Channel.Name;
+
+                await _discServer.UpdateChannel(discordserver);
+                await ReplyAsync($"Canal de notificação atualizado para {discordserver.NameChannel}");
+            }
+        }
+        catch (Exception err)
+        {
+            await ReplyAsync($"Error: {err.Message}");
+        }
+    }
 
     [Command("ChannelNotification")]
     public async Task ChannelNotification()
     {
-        Discordserver serverInfo =  await _discServer.GetChannel((long)Context.Guild.Id);
+        Discordserver serverInfo = await _discServer.GetChannel((long)Context.Guild.Id);
 
-        if(serverInfo is null)
+        if (serverInfo is null)
             await ReplyAsync("Esser Servidor ainda não possui um canal de notificação!! use o seguinte commando: !RegisterChannel");
         else
         {
-            SocketGuildChannel channel = Context.Guild.GetChannel((ulong)serverInfo.IdChanel);    
-            await ReplyAsync($"Canal de notificação: {channel.Name}!!");
-        }                        
+            SocketGuildChannel channel = Context.Guild.GetChannel((ulong)serverInfo.IdChanel);
+            await ReplyAsync($"Canal de notificação: {channel.Name}");
+        }
     }
 }
